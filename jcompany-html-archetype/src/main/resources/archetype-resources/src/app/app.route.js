30c8a0cc7586abeb2f331c23b91a4c6508ca7386
Rogerio Baldini <rogerio.baldini@powerlogic.com.br>
2015-11-06 14:21:25 -0200

    jcompany-html-archetype

--- jcompany-html-archetype/src/main/resources/archetype-resources/src/app/app.route.js
+++ jcompany-html-archetype/src/main/resources/archetype-resources/src/app/app.route.js
@@ -19,45 +19,8 @@
         controllerAs: 'inicial'
       });
 
-    $stateProvider
-      .state('departamentosel', {
-        url: '/departamentosel',
-        templateUrl: 'app/departamento/departamentosel.html',
-        controller: 'DepartamentoController',
-        controllerAs: 'departamentoController'
-      });
 
-    $stateProvider
-      .state('departamentoman', {
-        url: '/departamentoman/:id',
-        templateUrl: 'app/departamento/departamentoman.html',
-        controller: 'DepartamentoController',
-        controllerAs: 'departamentoController'
-    });
 
-    $stateProvider
-      .state('uf', {
-        url: '/uf',
-        templateUrl: 'app/uf/uf.html',
-        controller: 'ufController',
-        controllerAs: 'ufController'
-      });
-
-    $stateProvider
-      .state('funcionariosel', {
-        url: '/funcionariosel',
-        templateUrl: 'app/funcionario/funcionariosel.html',
-        controller: 'funcionarioController',
-        controllerAs: 'funcionarioController'
-      });
-
-    $stateProvider
-      .state('funcionariomdt', {
-        url: '/funcionariomdt/:id',
-        templateUrl: 'app/funcionario/funcionariomdt.html',
-        controller: 'funcionarioController',
-        controllerAs: 'funcionarioController'
-      });
 
     $urlRouterProvider.otherwise('/');
   }
