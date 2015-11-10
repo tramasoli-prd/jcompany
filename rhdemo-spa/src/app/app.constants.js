0617eb80baee279f483c09aa69f06e0c3b230355
Rogerio Baldini <rogerio.baldini@powerlogic.com.br>
2015-11-10 12:51:22 -0200

    Configurações para rodar em Weblogic, jBoss e Webphere

--- rhdemo-spa/src/app/app.constants.js
+++ rhdemo-spa/src/app/app.constants.js
@@ -9,7 +9,9 @@
 	angular
 	.module('jcompany-view')
 	.constant('$menuPath', 'app/components/json/menu.json')
+	/*.constant('$backendUrl', 'http://localhost:9080/rhdemo-service');*/
 	.constant('$backendUrl', 'http://localhost:7001/rhdemo-service');
+	/*.constant('$backendUrl', 'http://localhost:8080/rhdemo-service');*/
 
 	
 })();
