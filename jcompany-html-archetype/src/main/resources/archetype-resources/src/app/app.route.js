f53c59584195ecc1306b66cb3ce7b640143e9bb5
Leandro Lima <leandro.lima@cast.com.br>
2016-02-02 09:17:25 -0200

    ajustes no archetype para novo processo de autenticação e redirecionamento

--- jcompany-html-archetype/src/main/resources/archetype-resources/src/app/app.route.js
+++ jcompany-html-archetype/src/main/resources/archetype-resources/src/app/app.route.js
@@ -9,16 +9,40 @@
 	.config(RouterConfig);
 
 	/** @ngInject */
-	function RouterConfig($stateProvider, $urlRouterProvider) {
+	function RouterConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+
+		// the log-on screen
+		$stateProvider.state('login',{
+			url : '/plc-login',
+			templateUrl: 'app/login/login.html',
+			controller: 'PlcLoginController',
+			controllerAs: 'plcLoginController',
+			access: 'public'
+		})
+		
 
 		$stateProvider
-		.state('home', {
-			url: '/',
-			templateUrl: 'app/inicial/inicial.html'
+		.state('app', {
+			url: '/app',
+			templateUrl: 'app/app.html',
+			controller: 'AppController',
+			controllerAs: 'appController'
 		});
-
-		$urlRouterProvider.otherwise('/');
+		
+		$stateProvider
+		.state('app.inicial', {
+			url: '/inicial',
+			parent: 'app',
+			templateUrl: 'app/inicial/inicial.html',
+			controller: 'InicialController',
+			controllerAs: 'inicialController'
+		});
+		
+		
+		$locationProvider.html5Mode(true)
+		$urlRouterProvider.otherwise('/app/inicial');
 	}
+
 
 })();
 
---
+++ jcompany-html-archetype/src/main/resources/archetype-resources/src/app/inicial/inicial.controller.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+
+
+	angular
+	.module('rhdemo')
+	.controller('InicialController', InicialController);
+	
+	/** @ngInject */
+	function InicialController($scope, $cookies, $state, $window, PlcAuthService, $stateParams, PlcMenuLoader, PlcMenu) {
+		var vm = this;
+		
+
+	}
+
+})();
